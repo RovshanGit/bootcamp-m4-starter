aa8e0798678fa6e0e1df08cbc64ba57cf81ab5f7
RovshanGit <babayev.work@gmail.com>
2023-01-15 18:11:48 +0400

    Finished

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -6,18 +6,74 @@ import { Link } from 'react-router-dom';
 class Favorites extends Component {
     state = {
         title: 'Новый список',
-        movies: []
+        movies: [],
+        disabled: false,
+        id: "",
+        value: "",
+        countMovies:0
     }
+    handleChange=(e)=>{
+        this.setState({value: e.target.value})
+    }
+    deleteMovie(imdbID) {
+        store.dispatch({
+            type: "DELETE_MOVIE",
+            payload: {
+                imdbID: imdbID,
+            }
+        })
+    }
+
+    handleSave = (e) => {
+        this.setState({ disabled: true });
+        e.target.style.display = "none";
+        const data = {
+            "title": this.state.value,
+            "movies": this.state.movies
+        };
+        fetch(`https://acb-api.algoritmika.org/api/movies/list `, {
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json())
+            .then((res) => this.setState({ id: res.id }))
+
+
+    }
+    
+    componentDidMount() {
+        store.subscribe (() => {
+            const storeState = store.getState();
+            this.setState({movies: storeState.favorites, countMovies: storeState.countMovies})
+        })
+    }
+
     render() { 
+        const {value, disabled} = this.state;
         return (
             <div className="favorites">
-                <input value="Новый список" className="favorites__name" />
+                <input 
+                    disabled={disabled} 
+                    value={value} 
+                    onChange={this.handleChange} 
+                    placeholder="Новый список" 
+                    className="favorites__name" />
+
                 <ul className="favorites__list">
-                    {this.state.movies.map((item) => {
-                        return <li key={item.id}>{item.title} ({item.year})</li>;
-                    })}
+                    {this.state.movies.map((item) => 
+                        (<li key={item.imdbID} className="favorites__list_items" >({item.Year}) {item.Title}
+                        <button onClick={()=> this.deleteMovie(item.imdbID)}  disabled ={this.state.disabled}>X</button>
+                        </li>)
+                    )}
                 </ul>
-                <button type="button" className="favorites__save">Сохранить список</button>
+
+                {
+                !this.state.disabled ? (<button type="button" onClick={this.handleSave} 
+                disabled={this.state.value === "" || this.state.countMovies <= 0} className="favorites__save" >Сохранить список</button>)
+                 : (<Link to={"/list/" + this.state.id} >Список филмов</Link>)
+                } 
             </div>
         );
     }
